83f07b0f2f55821849801b78db79ff248b59e2e1
ZeroLog <YamaOG3@hotmail.com>
2024-07-01 15:54:18 +0700

    Added logic for selecting the first player

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 
 use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::net::TcpStream;
+
  
 fn main() -> std::io::Result<()> {
     let stream = TcpStream::connect("127.0.0.1:4545")?;
@@ -9,21 +10,41 @@ fn main() -> std::io::Result<()> {
     let stdin = io::stdin();
     let mut input = String::new();
     let mut response = String::new();
-     
-    loop {
+    reader.read_line(&mut response)?;
+    let r: i8 = response.trim().parse().unwrap();
+    response.clear();
 
+    loop {
+       
+        if r == 2{
+        println!("Ваш ход");
         input.clear();
         stdin.read_line(&mut input)?;
         writer.write_all(input.as_bytes())?;
         writer.flush()?;
-       
+        println!("Ждите хода соперника");
         response.clear();
         reader.read_line(&mut response)?;
         if response.is_empty() {
             break;
         }
         println!("Server: {}", response.trim());
-       
+         }
+
+        if r == 1{
+        println!("Ждите хода соперника");
+        response.clear();
+        reader.read_line(&mut response)?;
+        if response.is_empty() {
+            break;
+        }
+        println!("Server: {}", response.trim());
+        println!("Ваш ход");
+        input.clear();
+        stdin.read_line(&mut input)?;
+        writer.write_all(input.as_bytes())?;
+        writer.flush()?;
+    }
     }
 
     Ok(())
